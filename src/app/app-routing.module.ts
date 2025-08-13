7ba651d2b2a1ddb8fff2d572f677dd1e6be30fef
Marikrishnan-ADS <marikrishnan.m@alldirectionsource.com>
2025-08-13 12:02:31 +0530

    add-new-change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./landing-page/landing-page.module').then(m => m.LandingPageModule)
+  },
+  {
+    path: 'features',
+    loadChildren: () =>
+      import('./features/features.module').then(m => m.FeaturesModule)
+  },
+  { path: '**', redirectTo: '' } // wildcard route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
